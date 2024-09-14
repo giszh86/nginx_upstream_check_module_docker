fccd5ba7d3e8184d9fe16c3bd6b9b5e5775316d7
gis_zhong <gis_zhong@sina.com>
2024-09-14 10:52:29 +0800

    Update docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -1,5 +1,3 @@
 #! /usr/bin/env bash
 
-docker run -d --name=app1 -p 8080:80 nginx:alpine3.19
-docker run -d --name=app2 -p 8081:80 nginx:alpine3.19
-docker run -d --name=nginx --network=host local/nginx:1.26.1
+docker run -d --name=nginx --network=host joint/nginx:1.26.1
